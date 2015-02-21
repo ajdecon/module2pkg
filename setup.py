a01c7cba9e01f19ffef82d259ae8a6c7753ea2dd
Adam DeConinck <ajdecon@ajdecon.org>
2015-02-21 21:46:57 +0000

    Fix syntax error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='module2pkg',
       version='0.1',
-      description="Build an OS package from an environment module (and its dependencies)"
+      description="Build an OS package from an environment module (and its dependencies)",
       author="Adam DeConinck",
       author_email="ajdecon@ajdecon.org",
       url="http://www.github.com/ajdecon/module2pkg",
