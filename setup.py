400d49da47055c86236916fb61862b34b5399956
Adam DeConinck <ajdecon@ajdecon.org>
2015-02-21 21:46:18 +0000

    Add python packaging files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='module2pkg',
+      version='0.1',
+      description="Build an OS package from an environment module (and its dependencies)"
+      author="Adam DeConinck",
+      author_email="ajdecon@ajdecon.org",
+      url="http://www.github.com/ajdecon/module2pkg",
+      scripts=['module2pkg'])
